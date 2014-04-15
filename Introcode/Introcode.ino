c9d23ba83c3675c7beddfc24f5a9fabd2e6690ab
Jaewoo Kim <jaewook@cyh100-a8.cyert.andrew.cmu.edu>
2014-04-15 17:23:29 -0400

    Whatever

--- Introcode/Introcode.ino
+++ Introcode/Introcode.ino
@@ -1,5 +1,7 @@
 int led = 9;
 int brightness = 255;
+int amountofTime = 1
+int whatIsThisMagicNumber = 9600;
 
 void setup(){
   Serial.begin(9600);
@@ -8,6 +10,15 @@ void setup(){
 }
 
 void loop(){
+  Serial.println("look what this changed to" + String(foo, DEC);
+  foo = foo + bar;
+  if(foo >= 255 || foo <= 0){
+    bar = -bar;
+  }
   analogWrite(led, brightness);
   Serial.println("I'm setting the led to" + String(brightness, DEC);
+  
+  delay(amountofTime);
 }
+
+#https://github.com/vrazdan/CMU-ChemE-Car/commit/1fd18f0402e0c1291f2a586b75aa871ce3f4be93
